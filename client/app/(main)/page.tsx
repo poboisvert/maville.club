2f2532a4531cdae69ff5c7a628a9fe65a8aa22f0
PO Boisvert <pob944@hotmail.com>
2026-05-18 18:35:56 -0400

    add readme

--- client/app/(main)/page.tsx
+++ client/app/(main)/page.tsx
@@ -1,5 +1,13 @@
+import type { Metadata } from "next";
+
 import { Hero } from "@/components/hero";
 import { WhyChooseUs } from "@/components/why-choose-us";
+
+export const metadata: Metadata = {
+  alternates: {
+    canonical: "https://www.maville.club",
+  },
+};
 
 export default function Home() {
   return (
